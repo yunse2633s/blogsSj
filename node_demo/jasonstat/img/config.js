b725807c1de78ffc2847adfd6802b458ebac9d58
272462045 <272462045@qq.com>
2020-08-20 10:20:35 +0800

    优化代码：减少数据库请求，定时重置变量

---
+++ node_demo/jasonstat/img/config.js
@@ -0,0 +1,146 @@
+module.exports = {
+	"imgurl":"http://192.168.1.144/"
+	,"dburl": "mongodb://192.168.1.144:27017/Jlink2"
+	,"v_data" : {
+        "user_info": {
+            "sign_time": "",
+            "direction": "",
+            "name": "",
+            "work_type": "",
+            "device_name": "",
+            "sign_avatar": "",
+            "avatar": "",
+            "unit": ""
+        },
+        // "updateTime": 10,
+        // "updateDate":8,
+        "transit_trend": [
+            {
+                "h": "0",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "1",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "2",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "3",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "4",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "5",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "6",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "7",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "8",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "9",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "10",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "11",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "12",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "13",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "14",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "15",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "16",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "17",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "18",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "19",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "20",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "21",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "22",
+                "enter_count": "0",
+                "out_count": "0"
+            },
+            {
+                "h": "23",
+                "enter_count": "0",
+                "out_count": "0"
+            }
+        ],
+        "total_count": "0",
+        "out_count": "0",
+        "entry_count": "0",
+        "real_count": "0",
+        "companys": [
+        ]
+    }
+}
